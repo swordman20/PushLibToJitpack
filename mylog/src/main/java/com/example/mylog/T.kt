0d011e1025f2bc39466adc5951f4a06a38a1b4e4
Hades <xiaweifeng@live.cn>
2022-03-11 19:17:28 +0800

    Initial commit

---
+++ mylog/src/main/java/com/example/mylog/T.kt
@@ -0,0 +1,16 @@
+package com.example.mylog
+
+import android.util.Log
+
+/**
+ *    author : hades
+ *    date   : 2022/3/11
+ *    desc   :
+ */
+class T {
+    companion object{
+        fun loge(msg:String){
+            Log.e("T",msg)
+        }
+    }
+}
